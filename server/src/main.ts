0f13d8ff49229ec979b4b5c31202b98b41dc2cc0
Denis Prokhorenko <denis.prokhorenko1@gmail.com>
2026-02-13 20:22:47 +0300

    feat: init config, cookieParser, enableCors, prisma, redis, add utils, add dependencies

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,60 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { ConfigService } from '@nestjs/config';
+import cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
+import IORedis from 'ioredis';
+import session from 'express-session';
+import { ms, StringValue } from './libs/common/utils/ms-util';
+import { parseBoolean } from './libs/common/utils/parse-boolean.util';
+import { RedisStore } from 'connect-redis';
+
+import dotenv from 'dotenv';
+import dotenvExpand from 'dotenv-expand';
+
+dotenvExpand.expand(dotenv.config());
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = app.get(ConfigService);
+  const redis = new IORedis(config.getOrThrow<string>('REDIS_URI'));
+
+  app.use(cookieParser(config.getOrThrow<string>('COOKIES_SECRET')));
+
+  app.use(
+    session({
+      secret: config.getOrThrow<string>('SESSION_SECRET'),
+      name: config.getOrThrow<string>('SESSION_NAME'),
+      resave: true,
+      saveUninitialized: false,
+      cookie: {
+        domain: config.getOrThrow<string>('SESSION_DOMAIN'),
+        maxAge: ms(config.getOrThrow<StringValue>('SESSION_MAX_AGE')),
+        httpOnly: parseBoolean(config.getOrThrow<string>('SESSION_HTTP_ONLY')),
+        secure: parseBoolean(config.getOrThrow<string>('SESSION_SECURE')),
+        sameSite: 'lax',
+      },
+      store: new RedisStore({
+        client: redis,
+        prefix: config.getOrThrow<string>('SESSION_FOLDER'),
+      }),
+    }),
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
+    credentials: true,
+    exposedHeaders: ['set-cookie'],
+  });
+
+  await app.listen(config.getOrThrow<number>('APPLICATION_PORT'));
 }
 bootstrap();
---
+++ server/src/prisma/prisma.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-call
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-call
+    super({ adapter });
+  }
+
+  async onModuleInit(): Promise<void> {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    await this.$connect();
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    await this.$disconnect();
+  }
+}
