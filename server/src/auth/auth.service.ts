41a4294ef1e7761d5123b7f8fa82e3f0e3ba9f6d
Denis Prokhorenko <denis.prokhorenko1@gmail.com>
2026-02-14 15:36:57 +0300

    feat: add provider resource, oatuh implementation for google and yandex

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -12,12 +12,16 @@ import type { Request, Response } from 'express';
 import { LoginDto } from './dto/login.dto';
 import { verify } from 'argon2';
 import { ConfigService } from '@nestjs/config';
+import { ProviderService } from './provider/provider.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
   constructor(
+    private readonly prismaService: PrismaService,
     private readonly userService: UserService,
     private readonly configService: ConfigService,
+    private readonly providerService: ProviderService,
   ) {}
 
   async register(req: Request, dto: RegisterDto) {
@@ -57,6 +61,50 @@ export class AuthService {
       throw new UnauthorizedException(
         'Неверный пароль. Пожалуйста, попробуйте еще раз или восстановите пароль, если забыли его.',
       );
+    }
+
+    return this.saveSession(req, user);
+  }
+
+  async extractProfileFromCode(req: Request, provider: string, code: string) {
+    const providerInstance = this.providerService.findByService(provider);
+    const profile = await providerInstance?.findUserByCode(code);
+
+    const account = await this.prismaService.account.findFirst({
+      where: {
+        id: profile?.id,
+        provider: profile?.provider,
+      },
+    });
+
+    let user = account?.userId
+      ? await this.userService.findById(account.userId)
+      : null;
+
+    if (user) {
+      return this.saveSession(req, user);
+    }
+
+    user = await this.userService.create(
+      profile!.email,
+      '',
+      profile!.name,
+      profile!.picture,
+      AuthMethod[profile!.provider.toUpperCase()],
+      true,
+    );
+
+    if (!account) {
+      await this.prismaService.account.create({
+        data: {
+          userId: user.id,
+          type: 'oauth',
+          provider: profile!.provider,
+          accessToken: profile!.access_token,
+          refreshToken: profile!.refresh_token,
+          expiresAt: profile!.expires_at || 0,
+        },
+      });
     }
 
     return this.saveSession(req, user);
